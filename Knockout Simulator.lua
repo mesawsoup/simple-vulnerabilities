82a12c7ed83e090bc2ad5fc2cb044a0536b7934d
mesawsoup <wesbrayden07@gmail.com>
2025-10-16 10:29:39 +0800

    Create Knockout Simulator.lua

---
+++ Knockout Simulator.lua
@@ -0,0 +1,3 @@
+-- literally the most simplest
+game:GetService("ReplicatedStorage"):WaitForChild("Event"):WaitForChild("Train"):FireServer(math.huge)
+game:GetService("ReplicatedStorage"):WaitForChild("Event"):WaitForChild("WinGain"):FireServer(math.huge)
