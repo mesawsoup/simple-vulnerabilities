9a23a18d29b18773032457a10cb01ec01c2bf565
mesawsoup <wesbrayden07@gmail.com>
2025-10-30 16:59:33 +0800

    Update Knockout Simulator.lua

--- Knockout Simulator.lua
+++ Knockout Simulator.lua
@@ -1,3 +1,4 @@
+-- for this https://www.roblox.com/share?code=0975993fe77aa1439a7a5f83d9ea3f0f&type=ExperienceDetails&stamp=1761814766457
 -- literally the most simplest
 game:GetService("ReplicatedStorage"):WaitForChild("Event"):WaitForChild("Train"):FireServer(math.huge)
 game:GetService("ReplicatedStorage"):WaitForChild("Event"):WaitForChild("WinGain"):FireServer(math.huge)
