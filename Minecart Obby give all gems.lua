26ad082996ddb93e383e2185d0b7ea14e53e9071
mesawsoup <wesbrayden07@gmail.com>
2025-10-30 16:57:23 +0800

    Update and rename Minecraft Obby give all gems.lua to Minecart Obby give all gems.lua

--- Minecart Obby give all gems.lua
+++ Minecart Obby give all gems.lua
@@ -1,3 +1,5 @@
+--this https://www.roblox.com/share?code=bad6e419b7befe4abecf68a1b790ba85&type=ExperienceDetails&stamp=1761814634260
+
 --[[for _,v in workspace:GetChildren() do
   if v:IsA("MeshPart") and v.Name == "Diamond" then
     game.Players.LocalPlayer.Character.HumanoidRootPart:PivotTo(CFrame.new(v.Position))
