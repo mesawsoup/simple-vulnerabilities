c7eed798b672dece4b4718e0fc83138d183e87c9
mesawsoup <wesbrayden07@gmail.com>
2025-10-30 17:13:00 +0800

    Update Easy Obby.lua

--- Easy Obby.lua
+++ Easy Obby.lua
@@ -1,5 +1,8 @@
+-- if you dont know how to use getgenv() stay out
+getgenv().Toggle = true
+-- you can do getgenv().Toggle = not getgenv().Toggle and true or false
 game.RunService.Heartbeat:Connect(function()
---for i = 1, 1 do
+--for i = 1, 100 do
 for _,v in workspace:GetDescendants() do
   if v.Name == "Checkpoint" and v:IsA("Part") then
     firetouchinterest(v, game.Players.LocalPlayer.Character.HumanoidRootPart, 0)
